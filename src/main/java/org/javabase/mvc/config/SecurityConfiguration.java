da7fde085b4eb5b3a3d95e75441ccf98a104f9da
Saurav Wahid <saurav1161@gmail.com>
2016-09-03 22:08:15 +0600

    03SEP2016 10:08 PM

---
+++ src/main/java/org/javabase/mvc/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.javabase.mvc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author OITI
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+		@Autowired
+	    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+	        auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("USER");
+	    }
+	     
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	       
+	      http.authorizeRequests()
+	        .antMatchers("/", "/home").permitAll()
+	        .antMatchers("/admin/**").access("hasRole('ADMIN')")
+	        .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")
+	        .and().formLogin().loginPage("/login")
+	        .usernameParameter("username").passwordParameter("password")
+	        .and().csrf()
+	        .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+	    }
+}
---
+++ src/main/java/org/javabase/mvc/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package org.javabase.mvc.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
